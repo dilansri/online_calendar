aee88cf9a4cde53361843bcb4855d5d130639a35
Dilan Srilal <dilan.srilal@gtnexus.com>
2015-07-06 16:19:38 +0530

    Basic authentication added

---
+++ OnlineCalendarXfinity/src/main/java/com/xfinity/config/SecurityApplicationIntializer.java
@@ -0,0 +1,8 @@
+package com.xfinity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityApplicationIntializer extends
+		AbstractSecurityWebApplicationInitializer {
+
+}
