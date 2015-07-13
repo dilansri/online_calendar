b3e0d27465c1d9762fa7f8c9e2195a5516780ff6
Dilan Srilal <dilan.srilal@gtnexus.com>
2015-07-13 12:16:14 +0530

    Redirect when logged in.

--- OnlineCalendarXfinity/src/main/java/com/xfinity/controller/HomeController.java
+++ OnlineCalendarXfinity/src/main/java/com/xfinity/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.xfinity.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	@RequestMapping(method=RequestMethod.GET)	
-	public String index(Model model){
+	public String index(Model model){		
+		
+		if(SecurityContextHolder.getContext().getAuthentication() != null &&
+				SecurityContextHolder.getContext().getAuthentication().isAuthenticated())	{
+			return "redirect:/my/planner";
+		}
 		model.addAttribute("greeting","HELLO WORLDss From Home");
-			
 		return "hello";
 	}	
 
